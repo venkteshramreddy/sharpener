dbb86d7ece7a2df4fd98dc6f67db9334550d79b8
venktesh Reddy <venkteshramreddy@gmail.com>
2025-07-11 23:33:48 +0530

    add paragraph

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const fruits = document.getElementsByClassName('fruit');
+const mainHeading = document.querySelector('#main-heading');
+const subHeading = document.createElement('h3');
+subHeading.textContent = 'Buy high quality organic fruits online';
+subHeading.style.fontStyle = 'italic';
 
-fruits[2].style.backgroundColor = 'yellow';
 
-for (let i = 0; i < fruits.length; i++) {
-  fruits[i].style.fontWeight = 'bold';
-}
+mainHeading.parentNode.insertBefore(subHeading, mainHeading.nextSibling);
+
+
+const fruitList = document.querySelector('.fruits');
+const para = document.createElement('p');
+para.textContent = 'Total fruits: 4';
+para.id = 'fruits-total';
+
+fruitList.parentNode.insertBefore(para, fruitList);
