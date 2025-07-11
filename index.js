f854d037d5f62781f2ff30ad59ef2c8a42c6e0b3
venktesh Reddy <venkteshramreddy@gmail.com>
2025-07-11 19:51:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("main-heading").textContent = "Fruit World";
+
+document.getElementById("main-heading").style.color = "orange";
+   
+document.getElementById("header").style.backgroundColor = "green";
+
+document.getElementById("header").style.borderBottom = "2px solid orange";
+
+document.getElementById("basket-heading").style.color = "green";
+
+const p = document.createElement("p");
+p.textContent = "Please visit us again";
+document.getElementById("thanks").appendChild(p);
+
